79553db5d46fd97521e3b59d5d6d83b8d9757460
szkmsy <szkmsy.dev+github@gmail.com>
2023-02-01 08:40:37 +0900

    Get port no from env

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function getHtml(url = "https://example.com") {
 }
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT ?? 8080;
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
